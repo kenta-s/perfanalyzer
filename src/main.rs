83894273f1a426829b3d50b0dfca4564d5eace10
kenta-s <knt01222@gmail.com>
2020-12-25 14:56:00 +0900

    use match

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-// use std::fs::File;
-// use std::io::{self, BufRead, BufReader};
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 use regex::Regex;
 use std::collections::HashMap;
 
@@ -11,9 +11,16 @@ struct PerfInfo {
     duration: f32,
     view: f32,
     db: f32,
+    count: u32
 }
 
-fn row_to_perf_info(row: &str) -> PerfInfo {
+struct SlowPage {
+    path: String,
+    average_duration: f32,
+    count: u32
+}
+
+fn row_to_perf_info(row: &str) -> std::io::Result<PerfInfo> {
     let path_regex = Regex::new(r"path=(\S+)").unwrap();
     let path_cap = path_regex.captures(row).unwrap();
 
@@ -42,7 +49,7 @@ fn row_to_perf_info(row: &str) -> PerfInfo {
     let db_cap_split: Vec<&str> = db_cap[0].split("=").collect();
     let db = db_cap_split[1].parse::<f32>().unwrap();
 
-    return PerfInfo {
+    let perf_info = PerfInfo {
         method: String::from(&method_cap[0]),
         path: String::from(&path_cap[0]),
         controller: String::from(&controller_cap[0]),
@@ -50,31 +57,54 @@ fn row_to_perf_info(row: &str) -> PerfInfo {
         duration: duration,
         view: view,
         db: db,
+        count: 1
     };
+
+    return Ok(perf_info);
 }
 
 fn main() -> std::io::Result<()> {
     let mut perf_map: HashMap<&str, PerfInfo> = HashMap::new();
-    let mut existing_keys = Vec::new();
-    let row = "[dc4889ee-3ff7-4fea-9c67-a184de666108] method=GET path=/sign_in format=html controller=Admin::SessionsController action=new status=200 duration=15867.41 view=15866.25 db=0.00 host=DESKTOP-SVH2LA9 remote_ip=::1 request_host=localhost params={}";
-    let path_regex = Regex::new(r"path=(\S+)").unwrap();
-    if path_regex.is_match(row) {
-      let perf_info = row_to_perf_info(row);
+    // let mut existing_keys = Vec::new();
 
-      let key = format!("{} {}", perf_info.method, perf_info.path);
-      existing_keys.push(key.as_str());
-      let value = perf_map.get(key.as_str());
-      match value {
-         None => perf_map.insert(key.as_str(), perf_info),
-         _ => perf_map.insert(key.as_str(), perf_info) // TODO: insert perf_info with longer duration
-      };
+    // TODO: get filename from ARGV
+    for result in BufReader::new(File::open("./development.log")?).lines() {
+        let row = result?;
 
-      // println!("{}", bar.method);
-      // println!("{}", bar.path);
-      // println!("{}", bar.duration);
-      // println!("{}", bar.view);
-      // println!("{}", bar.db);
-    };
+        let path_regex = Regex::new(r"path=(\S+)").unwrap();
+
+        if path_regex.is_match(&row) {
+          let perf_result = row_to_perf_info(&row);
+          // println!("{}", &row);
+
+          match perf_result {
+            Err(why) => println!("{}", why),
+            Ok(perf) => println!("{}", perf.method),
+          }
+
+          // let perf_info = match perf_result {
+          //   Err(why) => None,
+          //   Some(&perf) => Ok(perf,
+          // };
+          // println!("{} {}", perf_info.method, perf_info.path);
+
+          // let key = format!("{} {}", perf_info.method, perf_info.path);
+          // let value = perf_map.get(key.as_str());
+          // println!("{}", value.path)
+          // // existing_keys.push(key.as_str());
+          // match value {
+          //    None => perf_map.insert(key.as_str(), perf_info),
+          //    _ => perf_map.insert(key.as_str(), perf_info) // TODO: insert perf_info with longer duration
+          // };
+
+        };
+    }
+
+    // println!("{}", bar.method);
+    // println!("{}", bar.path);
+    // println!("{}", bar.duration);
+    // println!("{}", bar.view);
+    // println!("{}", bar.db);
 
     // for x in &existing_keys {
     //     println!("{}", x);
